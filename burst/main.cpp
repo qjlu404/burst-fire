f6514e36b802466492116eb958977d0adb6bc0de
Quentin Upshaw <qjlu404@gmail.com>
2021-07-16 16:52:09 -0500

    added txt file for easy config

--- burst/main.cpp
+++ burst/main.cpp
@@ -1,12 +1,16 @@
 #include <windows.h>
+#include <fstream>
 #include <iostream>
 #include <time.h>
+std::fstream hfile;
+int ms, ms1 = 0;
 void burst()
 {
 	clock_t startTime = clock();
-	while ((double(clock() - startTime) / (double)CLOCKS_PER_SEC) < 0.250)
+	while (clock() - startTime < ms)
 	{
 		mouse_event(MOUSEEVENTF_LEFTDOWN, NULL, NULL, NULL, NULL);
+		Sleep(50);
 	}
 	
 		Sleep(10);
@@ -14,10 +18,19 @@ void burst()
 }
 int main()
 {
+	
 	while (true)
 	{
-		if (GetAsyncKeyState(VK_LBUTTON) < 0)
+		if (GetAsyncKeyState(0x43) < 0)
 		{
+			hfile = std::fstream("test.txt", std::ios::in | std::ios::out);
+			if (hfile.is_open())
+			{
+				hfile >> ms;
+				hfile >> ms1;
+				hfile.close();
+
+			}
 			burst();
 		}
 	}
