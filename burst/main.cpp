e689e9726277d5bbf57295c316de89d61edc5926
Quentin Upshaw <qjlu404@gmail.com>
2021-07-16 17:29:40 -0500

    added a var

--- burst/main.cpp
+++ burst/main.cpp
@@ -3,35 +3,52 @@
 #include <iostream>
 #include <time.h>
 std::fstream hfile;
-int ms, ms1 = 0;
+int ms, ms1, ch = 0;
 void burst()
 {
 	clock_t startTime = clock();
 	while (clock() - startTime < ms)
 	{
-		mouse_event(MOUSEEVENTF_LEFTDOWN, NULL, NULL, NULL, NULL);
-		Sleep(50);
+		if (ch == 1)
+			mouse_event(MOUSEEVENTF_LEFTDOWN, NULL, NULL, NULL, NULL);
+		if (ch == 0)
+		{
+			mouse_event(MOUSEEVENTF_LEFTDOWN, NULL, NULL, NULL, NULL);
+			Sleep(10);
+			mouse_event(MOUSEEVENTF_LEFTUP, NULL, NULL, NULL, NULL);
+			Sleep(ms1);
+		}
+		Sleep(ms1);
 	}
-	
-		Sleep(10);
+	Sleep(10);
+	if(ch == 1)
 		mouse_event(MOUSEEVENTF_LEFTUP, NULL, NULL, NULL, NULL);
 }
 int main()
 {
-	
+	hfile = std::fstream("test.txt", std::ios::in | std::ios::out);
+	if (hfile.is_open())
+	{
+		hfile >> ms;
+		hfile >> ms1;
+		hfile >> ch;
+		hfile.close();
+
+	}
 	while (true)
 	{
-		if (GetAsyncKeyState(0x43) < 0)
+		if (GetAsyncKeyState(VK_LBUTTON) < 0)
 		{
+			burst();
 			hfile = std::fstream("test.txt", std::ios::in | std::ios::out);
 			if (hfile.is_open())
 			{
 				hfile >> ms;
 				hfile >> ms1;
+				hfile >> ch;
 				hfile.close();
 
 			}
-			burst();
 		}
 	}
 }
